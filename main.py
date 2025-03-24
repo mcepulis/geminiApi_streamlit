624968d5c8912a9e49d8945970f8f4676ec6f40b
mcepulis <109752570+mcepulis@users.noreply.github.com>
2025-03-24 12:55:01 +0200

    AI explain photo

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from geminiapi-streamlit!")
+import os
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+from rich import print
+
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(
+        api_key=gemini_api_key
+    )
 
 
-if __name__ == "__main__":
-    main()
+with open("./image/photo.jpg", "rb") as f:
+    image = f.read()
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=[
+        types.Part.from_bytes(data=image, mime_type="image/png"),
+        "THis is me. What popular place I visited? Answer in Lithuanian language.",
+    ],
+    
+)
+
+print(response.text)
